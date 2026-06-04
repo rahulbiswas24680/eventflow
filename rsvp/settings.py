1b34ad922cf419671a127d1191796196e4f40534
rahulbiswas <rahulbiswas1682001@gmail.com>
2026-06-04 17:52:55 +0530

    fixed package issues

--- rsvp/settings.py
+++ rsvp/settings.py
@@ -181,8 +181,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-STATIC_ROOT = "static/"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
